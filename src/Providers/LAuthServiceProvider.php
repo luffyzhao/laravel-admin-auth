faf0f8f430d11e8e564e02aa34741acc8ca33181
luffyzhao <luffyzhao@vip.126.com>
2019-09-18 16:39:18 +0800

    路径问题

--- src/Providers/LAuthServiceProvider.php
+++ src/Providers/LAuthServiceProvider.php
@@ -11,11 +11,7 @@ class LAuthServiceProvider extends RouteServiceProvider
 {
 
     public function boot(){
-        $this->publishes([
-            __DIR__.'/../Pulishes/resources/Vue' => resource_path('vue'),
-        ]);
-
-        $this->loadRoutesFrom(__DIR__.'/routes.php');
+        $this->loadRoutesFrom(__DIR__.'/../routes.php');
 
         $this->loadMigrationsFrom(__DIR__.'/../Pulishes/resources/migrations');
     }
