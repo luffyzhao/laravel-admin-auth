c4bc2cdd66d60a7a7b1595626b35feadb54bd712
luffyzhao <luffyzhao@vip.126.com>
2022-07-03 21:59:21 +0800

    vue 3 bug 修复

--- src/Pulishes/resources/vue/src/views/manage/authorities/menu/create.vue
+++ src/Pulishes/resources/vue/src/views/manage/authorities/menu/create.vue
@@ -1,104 +1,105 @@
 <template>
-    <div class="veu-create">
-        <div class="header">
-            <Spin v-if="loading" fix></Spin>
-            <Form ref="formCreate" :model="data" label-position="top" :rules="ruleValidate">
-                <Row :gutter="10">
-                    <Col span="12">
-                        <FormItem label="上级菜单">
-                            <Input :model-value="props.title" disabled></Input>
-                        </FormItem>
-                    </Col>
-                    <Col span="12">
-                        <FormItem label="菜单标题" prop="title">
-                            <Input v-model="data.title"></Input>
-                        </FormItem>
-                    </Col>
-                    <Col span="12">
-                        <FormItem label="菜单名称" prop="name">
-                            <Input v-model="data.name"></Input>
-                        </FormItem>
-                    </Col>
-                    <Col span="12">
-                        <FormItem label="菜单图标" prop="icon">
-                            <Input v-model="data.icon"></Input>
-                        </FormItem>
-                    </Col>
-                </Row>
-                <FormItem label="排序" prop="sort">
-                    <Input v-model="data.sort" number></Input>
-                </FormItem>
-                <FormItem label="菜单描述" prop="description">
-                    <Input v-model="data.description" type="textarea" :rows="4"></Input>
-                </FormItem>
-                <FormItem label="分配权限">
-                    <Transfer
-                            :titles="['可分配权限', '已有权限']"
-                            :list-style="{width: '300px',height: '500px'}"
-                            :data="authorities.data"
-                            :target-keys="data.authorities"
-                            @on-change="handleChange"></Transfer>
-                </FormItem>
-            </Form>
-        </div>
-        <div class="footer">
-            <Button type="primary" icon="ios-add" @click="submit('formCreate')">提交</Button>
-        </div>
+  <div class="veu-create">
+    <div class="header">
+      <Spin v-if="loading" fix></Spin>
+      <Form ref="formCreate" :model="data" label-position="top" :rules="ruleValidate">
+        <Row :gutter="10">
+          <Col span="12">
+            <FormItem label="上级菜单">
+              <Input :model-value="props.title" disabled></Input>
+            </FormItem>
+          </Col>
+          <Col span="12">
+            <FormItem label="菜单标题" prop="title">
+              <Input v-model="data.title"></Input>
+            </FormItem>
+          </Col>
+          <Col span="12">
+            <FormItem label="菜单名称" prop="name">
+              <Input v-model="data.name"></Input>
+            </FormItem>
+          </Col>
+          <Col span="12">
+            <FormItem label="菜单图标" prop="icon">
+              <Input v-model="data.icon"></Input>
+            </FormItem>
+          </Col>
+        </Row>
+        <FormItem label="排序" prop="sort">
+          <Input v-model="data.sort" number></Input>
+        </FormItem>
+        <FormItem label="菜单描述" prop="description">
+          <Input v-model="data.description" type="textarea" :rows="4"></Input>
+        </FormItem>
+        <FormItem label="分配权限">
+          <Transfer
+              :titles="['可分配权限', '已有权限']"
+              :list-style="{width: '300px',height: '500px'}"
+              :data="authorities.data"
+              :target-keys="data.authorities"
+              @on-change="handleChange"></Transfer>
+        </FormItem>
+      </Form>
     </div>
+    <div class="footer">
+      <Button type="primary" icon="ios-add" @click="submit('formCreate')">提交</Button>
+    </div>
+  </div>
 </template>
 
 <script>
 
-    import Menu from './data';
-    import CommonMixin from "@/mixins/common";
+import Menu from './data';
+import CommonMixin from "@/mixins/common";
 
-    export default {
-        name: "create",
-        mixins: [Menu, CommonMixin],
-        mounted(){
-            this.loading = true;
-            this.$http.get(`authorities/menu/create`).then((res) => {
-                this.authorities.data = res
-            }).finally(() => {
-                this.loading = false
-            });
-        },
-        methods: {
-            submit(name) {
-                this.validate(name).then(() => {
-                    this.loading = true;
-                    this.data.parent_id = this.props.id;
-                    this.$http.post(`authorities/menu`, this.data).then(() => {
-                        this.$store.dispatch('layout/remove', this.$route);
-                    }).finally(() => {
-                        this.loading = false;
-                        this.$emit('on-refresh');
-                    });
-                }).catch(() => {
+export default {
+  name: "create",
+  mixins: [Menu, CommonMixin],
+  mounted() {
+    this.loading = true;
+    this.$http.get(`authorities/menu/create`).then((res) => {
+      this.authorities.data = res
+    }).finally(() => {
+      this.loading = false
+    });
+  },
+  methods: {
+    submit(name) {
+      this.validate(name).then(() => {
+        this.loading = true;
+        this.data.parent_id = this.props.id;
+        this.$http.post(`authorities/menu`, this.data).then(() => {
+          this.show = false;
+        }).finally(() => {
+          this.loading = false;
+          this.$emit('on-refresh');
+        });
+      }).catch(() => {
 
-                });
-            }
-        }
+      });
     }
+  }
+}
 </script>
 
 <style scoped lang="less">
-.veu-create{
-    height: 100%;
+.veu-create {
+  height: 100%;
 
-    position: relative;
+  position: relative;
 
-    .header{
-        height: calc(100% - 40px);
-        overflow-y: scroll;
-        padding: 0 5px;
-        border-bottom: 1px solid #e8eaec;
-    }
-    .footer{
-        text-align: right;
-        height: 50px;
-        padding: 15px 15px 0 0;
-    }
+  .header {
+    height: calc(100% - 40px);
+    overflow-y: scroll;
+    padding: 0 5px;
+    border-bottom: 1px solid #e8eaec;
+  }
+
+  .footer {
+    text-align: right;
+    height: 50px;
+    padding: 15px 15px 0 0;
+  }
 }
 
 </style>
--- src/Pulishes/resources/vue/src/views/manage/authorities/menu/index.vue
+++ src/Pulishes/resources/vue/src/views/manage/authorities/menu/index.vue
@@ -1,40 +1,40 @@
 <template>
-    <BodyContent v-model="component">
-        <Row class="cards" :gutter="20">
-            <Col :span="7">
-                <Card>
-                    <template #title>
-                        <Dropdown @on-click="createMenu" class="dropdown-button">
-                            <Button size="small">
-                                添加菜单
-                                <Icon type="ios-arrow-down"></Icon>
-                            </Button>
-                            <template #list>
-                                <DropdownMenu>
-                                    <DropdownItem name="parent">添加顶级菜单</DropdownItem>
-                                    <DropdownItem :disabled="!Boolean(selectNode.id)" name="children">添加子菜单
-                                    </DropdownItem>
-                                </DropdownMenu>
-                            </template>
-                        </Dropdown>
-                        <Button size="small" type="error" v-if="checkNodes.length > 0" @click="remove">
-                            <Icon type="ios-clipboard"/>
-                            批量删除
-                        </Button>
-                    </template>
-                    <Tree :data="data" show-checkbox @on-select-change="selectChange"
-                          @on-check-change="checkChange"></Tree>
-                </Card>
-            </Col>
-            <Col :span="17">
-                <Card>
-                    <template #title>{{ title }}</template>
-                    <component v-if="component1.render !== null" :is="component1.render"
-                               :props="component1.props"></component>
-                </Card>
-            </Col>
-        </Row>
-    </BodyContent>
+  <BodyContent v-model="component">
+    <Row class="cards" :gutter="20">
+      <Col :span="7">
+        <Card>
+          <template #title>
+            <Dropdown @on-click="createMenu" class="dropdown-button">
+              <Button size="small">
+                添加菜单
+                <Icon type="ios-arrow-down"></Icon>
+              </Button>
+              <template #list>
+                <DropdownMenu>
+                  <DropdownItem name="parent">添加顶级菜单</DropdownItem>
+                  <DropdownItem :disabled="!Boolean(selectNode.id)" name="children">添加子菜单
+                  </DropdownItem>
+                </DropdownMenu>
+              </template>
+            </Dropdown>
+            <Button size="small" type="error" v-if="checkNodes.length > 0" @click="remove">
+              <Icon type="ios-clipboard"/>
+              批量删除
+            </Button>
+          </template>
+          <Tree :data="data" show-checkbox @on-select-change="selectChange"
+                @on-check-change="checkChange"></Tree>
+        </Card>
+      </Col>
+      <Col :span="17">
+        <Card>
+          <template #title>{{ title }}</template>
+          <component v-if="component1.render !== null" :is="component1.render"
+                     :props="component1.props"></component>
+        </Card>
+      </Col>
+    </Row>
+  </BodyContent>
 </template>
 
 <script>
@@ -47,105 +47,105 @@ import ICreate from "./create"
 import {markRaw} from "vue";
 
 export default {
-    name: "index",
-    mixins: [CommonMixin],
-    components: {BodyTableSearch, BodyTable, BodyContent, IUpdate, ICreate},
-    data() {
-        return {
-            title: '编辑菜单',
-            data: [],
-            selectNode: {},
-            checkNodes: [],
-            component1: {
-                render: null,
-                props: {},
-            },
-        }
-    },
-    mounted() {
-        this.getLists();
-    },
-    methods: {
-        openComponent(render, props) {
-            this.component1.render = null;
-            this.component1.render = markRaw(render);
-            this.component1.props = props;
-        },
-        createMenu(name) {
-            switch (name) {
-                case 'parent':
-                    this.openComponent('ICreate', {id: 0, title: '顶级菜单'});
-                    this.title = '添加顶级菜单';
-                    break;
-                case 'children' :
-                    this.openComponent('ICreate', this.selectNode);
-                    this.title = '添加[' + this.selectNode.title + ']子菜单';
-                    break;
-            }
-        },
-        remove() {
-            this.loading = true;
-            let nodes = this.checkNodes.map(val => val.id);
-            this.$http.delete(`authorities/menu/${nodes.join('|')}`).then((res) => {
-                this.getLists();
-            }).finally(() => {
-                this.loading = true
-            });
-        },
-        getLists() {
-            this.loading = true;
-            this.$http.get(`authorities/menu`).then((data) => {
-                this.data = this.setTreeData(data);
-                this.checkNodes = [];
-                this.selectNode = {};
-            }).finally(() => {
-                this.loading = false;
-            });
-        },
-        selectChange(nodes, node) {
-            if (nodes.length === 1) {
-                this.openComponent(IUpdate, nodes[0]);
-                this.title = '修改[' + nodes[0].title + ']菜单';
-                this.selectNode = nodes[0];
-            }
-        },
-        checkChange(nodes, node) {
-            this.checkNodes = nodes;
-        },
-        setTreeData(source) {
-            let cloneData = JSON.parse(JSON.stringify(source))
-            let tree = cloneData.filter(father => {
-                father.expand = true;
-                let branchArr = cloneData.filter(child => {
-                    return father['id'] == child['parent_id']
-                });
-                if (branchArr.length > 0) {
-                    father['children'] = branchArr
-                }
-                return father['parent_id'] == 0
-            })
-            return tree
-        }
+  name: "index",
+  mixins: [CommonMixin],
+  components: {BodyTableSearch, BodyTable, BodyContent, IUpdate, ICreate},
+  data() {
+    return {
+      title: '编辑菜单',
+      data: [],
+      selectNode: {},
+      checkNodes: [],
+      component1: {
+        render: null,
+        props: {},
+      },
     }
+  },
+  mounted() {
+    this.getLists();
+  },
+  methods: {
+    openComponent(render, props) {
+      this.component1.render = null;
+      this.component1.render = markRaw(render);
+      this.component1.props = props;
+    },
+    createMenu(name) {
+      switch (name) {
+        case 'parent':
+          this.openComponent(ICreate, {id: 0, title: '顶级菜单'});
+          this.title = '添加顶级菜单';
+          break;
+        case 'children' :
+          this.openComponent(ICreate, this.selectNode);
+          this.title = '添加[' + this.selectNode.title + ']子菜单';
+          break;
+      }
+    },
+    remove() {
+      this.loading = true;
+      let nodes = this.checkNodes.map(val => val.id);
+      this.$http.delete(`authorities/menu/${nodes.join('|')}`).then((res) => {
+        this.getLists();
+      }).finally(() => {
+        this.loading = true
+      });
+    },
+    getLists() {
+      this.loading = true;
+      this.$http.get(`authorities/menu`).then((data) => {
+        this.data = this.setTreeData(data);
+        this.checkNodes = [];
+        this.selectNode = {};
+      }).finally(() => {
+        this.loading = false;
+      });
+    },
+    selectChange(nodes, node) {
+      if (nodes.length === 1) {
+        this.openComponent(IUpdate, nodes[0]);
+        this.title = '修改[' + nodes[0].title + ']菜单';
+        this.selectNode = nodes[0];
+      }
+    },
+    checkChange(nodes, node) {
+      this.checkNodes = nodes;
+    },
+    setTreeData(source) {
+      let cloneData = JSON.parse(JSON.stringify(source))
+      let tree = cloneData.filter(father => {
+        father.expand = true;
+        let branchArr = cloneData.filter(child => {
+          return father['id'] == child['parent_id']
+        });
+        if (branchArr.length > 0) {
+          father['children'] = branchArr
+        }
+        return father['parent_id'] == 0
+      })
+      return tree
+    }
+  }
 }
 </script>
 
 
 <style lang="less">
 .cards {
+  height: 100%;
+  .dropdown-button{
+    margin-right: 10px;
+  }
+  .ivu-col {
     height: 100%;
-    .dropdown-button{
-        margin-right: 10px;
-    }
-    .ivu-col {
-        height: 100%;
 
-        .ivu-card{
-            height: 100%;
-            .ivu-card-body{
-                height: calc(100% - 53px);
-            }
-        }
+    .ivu-card{
+      height: 100%;
+      .ivu-card-body{
+        height: calc(100% - 53px);
+      }
     }
+  }
 }
 </style>
